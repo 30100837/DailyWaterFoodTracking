fe799b422b9227606810ac621af20130c02cee56
Md Tasnim Hasan <30100837@students.southwales.ac.uk>
2026-02-18 22:23:24 +0000

    first commit

---
+++ DailyWaterFoodTracker Watch App/DailyWaterFoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWaterFoodTrackerApp.swift
+//  DailyWaterFoodTracker Watch App
+//
+//  Created by Junayed Al Aditto on 18/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyWaterFoodTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# DailyWaterFoodTracking
+# DailyWaterFoodTracking
